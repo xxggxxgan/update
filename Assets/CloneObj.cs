93fc1c6d153006a2fd26224833c7e8633773f695
xxggxxgan <xxggxxgan@gmail.com>
2018-01-07 11:29:46 +0800

    shoot the ball

---
+++ Assets/CloneObj.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;  
+using UnityEngine;  
+
+public class CloneObj : MonoBehaviour {  
+
+	public GameObject spritePre;    //预制体  
+	public Rigidbody Bullet;
+	GameObject cloneObj;    //克隆出的对象  
+	public  Transform  FPonit;
+
+	// Use this for initialization  
+	void Start () {  
+
+
+		cloneObj = Instantiate(spritePre) as GameObject;      
+		cloneObj.transform.parent = transform;  
+		//cloneObj.transform.localPosition = new Vector3((i % 5-2) * 100, (i / 5) * 100, 0);  
+			//5s后销毁对象  
+		Destroy(cloneObj, 3); 
+		  
+	}
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.J)){
+			cloneObj = Instantiate(spritePre) as GameObject;      
+			Destroy (cloneObj, 1);
+		}
+	}
+}
---
+++ Assets/Shoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+	int speed = 20;
+	public  Rigidbody Bullet;
+
+	public  Transform  FPonit;
+
+	public 	Rigidbody  Player;
+	// Use this for initialization
+	void Start () {
+		Player = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.J)){
+			Rigidbody clone;
+			clone = (Rigidbody)Instantiate (Bullet, FPonit.position, FPonit.rotation);
+			clone.velocity = transform.TransformDirection (Vector3.forward * 50);
+		}
+	}
+
+	void Destroy(){
+		
+
+
+	}
+
+
+}
